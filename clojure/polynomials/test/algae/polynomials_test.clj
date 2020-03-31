72f1328257184fb960093a6905e78ee5620de1d3
Bobby Towers <porkostomus@mail.com>
2020-03-31 00:33:46 -0700

    don't test string formatting

--- clojure/polynomials/test/algae/polynomials_test.clj
+++ clojure/polynomials/test/algae/polynomials_test.clj
@@ -4,11 +4,11 @@
 
 (deftest add-and-subtract-test
   (testing "Subtract polynomials."
-    (is (= (print-poly (sub-poly [-9 0 0 0 8] [-9 2 5 0 0]))
-           " -2x^3 -5x^2  8 "))
-    (is (= (print-poly (sub-poly [6 2 5] [5 -6 -11]))
-           "1x^2 8x 16 " ))
-    (is (= (print-poly (sub-poly [-7 3 -6] [3 4 4]))
-           "-10x^2 -1x -10 "))
-    (is (= (print-poly (sub-poly [1 8 -9] [11 -4 7]))
-           "-10x^2 12x -16 "))))
+    (is (= (sub-poly [-9 0 0 0 8] [-9 2 5 0 0])
+           '(0 -2 -5 0 8)))
+    (is (= (sub-poly [6 2 5] [5 -6 -11])
+           '(1 8 16) ))
+    (is (= (sub-poly [-7 3 -6] [3 4 4])
+           '(-10 -1 -10)))
+    (is (= (sub-poly [1 8 -9] [11 -4 7])
+           '(-10 12 -16)))))
